f50b83d7a7fda540084c45d4c07d0d9331a59ce6
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-28 19:54:42 +0100

    imagen

--- BlogPHP/Model/News.php
+++ BlogPHP/Model/News.php
@@ -6,8 +6,16 @@ class News {
 	private $_title;
 	private $_content;
 	private $_date;
-	private $_image;
+	private $_imagen;
 	
+	public function setAll($id=0, $idAuthor=0, $title="", $content="", $date=null, $imagen=null) {
+		$this->_id=$id;
+		$this->_idAuthor=$idAuthor;
+		$this->_title=$title;
+		$this->_content=$content;
+		$this->_date=$date;
+		$this->_imagen=$imagen;
+	}
 	
 	public function __get($property) {
 		if (property_exists($this, $property)) {
