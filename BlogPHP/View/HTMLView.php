ac83f521d716933bca193f2313fda2a451c5df2f
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-28 18:04:33 +0100

    HTML VIEW NAVBAR

--- BlogPHP/View/HTMLView.php
+++ BlogPHP/View/HTMLView.php
@@ -27,11 +27,36 @@ class HTMLView {
 		    <![endif]-->
 		  </head>
 		  <body>
+		  <nav class="navbar navbar-default navbar-fixed-top">
+			  <div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			      <a class="navbar-brand" href="index.php">Blog CMS by Michal Sobocinski</a>
+			    </div>
+			
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      <ul class="nav navbar-nav">
+			        <li><a href="index.php">Blog <span class="sr-only">(current)</span></a></li>
+			        <li><a href="#">Añadir <noticia></noticia></a></li>
+			      </ul>
+			    </div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		  
+		  <div class="content">
 <?php
 	}
 	
 	public static function footer() {
 		?>
+			</div>
 			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		    <!-- Include all compiled plugins (below), or include individual files as needed -->
